f25df07aeb36112235367b39015a8bdc77b3079c
Nukaze <nukaze4@gmail.com>
2024-09-22 22:26:47 +0700

    refactor: add comment guide in each code

--- src/main.py
+++ src/main.py
@@ -3,19 +3,29 @@ from ultralytics import YOLO
 import torch
 
 def main():
+    # Load the YOLO model
     model = YOLO("models/yolov8s.pt")
+    
+    # Check if CUDA is available and set the model to CUDA if it is
     if (is_cuda_available()):
         model.to("cuda")
-        print("model is processing on cuda")
+        print("="*64)
+        print("*** Model is processing on [CUDA] ***")
+        print("="*64)
     else:
         model.to("cpu")
-        print("model is processing on cpu")
-        
+        print("="*64)
+        print("*** Model is processing on [CPU] ***")
+        print("="*64)
+    
+    # Set the source to the camera
     camera_index = 1
+    
+    # Start the AI model prediction
     result = model.predict(source=camera_index, conf=.5, show=True, stream=True)
     
+    # Loop through the results for each frame
     for res in result:
-        
         boxes = res.boxes
         classes = boxes.cls.tolist()
         conf = [round(v, 2) for v in boxes.conf.tolist()]
@@ -28,8 +38,10 @@ def main():
         
     
 def is_cuda_available():
+    # Check if CUDA is available
     return torch.cuda.is_available()
     
-    
+
+
 if __name__ == '__main__':
     main()
