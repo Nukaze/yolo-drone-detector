26dbc168ac4d2ffaaf40a8e76fec6080d45c5d9c
Nukaze <nukaze4@gmail.com>
2024-09-26 17:52:34 +0700

    feat: major model config, throbber anim, threading

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import threading
+import time
 import cv2
 import ultralytics
 from ultralytics import YOLO
 
 import torch
 
-def main():
+def main() -> None:
     # Check the version of the ultralytics
     ultralytics.checks()
     # Load the YOLO model
@@ -21,31 +23,64 @@ def main():
         print("="*64)
         print("*** Model is processing on [CPU] ***")
         print("="*64)
+        
+    stop_trobber_event = threading.Event()    
+    throbber_thread = threading.Thread(target=throbber_animation, args=(stop_trobber_event,))
+    throbber_thread.start()
+    
     
     # Set the source to the camera
-    camera_index = 1
+    camera_index = 2
     
-    # Start the AI model prediction
-    result = model.predict(source=camera_index, conf=.5, show=True, stream=True)
-    
-    # Loop through the results for each frame
-    for res in result:
-        boxes = res.boxes
-        classes = boxes.cls.tolist()
-        conf = [round(v, 2) for v in boxes.conf.tolist()]
-        xyxy = boxes.xyxy.tolist()
-        origin_shape = tuple(boxes.orig_shape)
-        xywh = boxes.xywh.tolist()
-        xywhn = boxes.xywhn.tolist()
-        xyxy = boxes.xyxy.tolist()
-        xyxyn = boxes.xyxyn.tolist()
+    try:
+        # Start the AI model prediction
+        result = model.track(
+            source=camera_index,
+            classes=[4,],
+            show_labels=True,
+            show_conf=True,
+            conf=0.5,
+            show=True,
+            stream=True,
+            verbose=False
+            save_txt=True,
+        )
         
+        # Loop through the results for each frame
+        for res in result:
+            boxes = res.boxes
+            classes = boxes.cls.tolist()
+            conf = [round(v, 2) for v in boxes.conf.tolist()]
+            xyxy = boxes.xyxy.tolist()
+            origin_shape = tuple(boxes.orig_shape)
+            xywh = boxes.xywh.tolist()
+            xywhn = boxes.xywhn.tolist()
+            xyxy = boxes.xyxy.tolist()
+            xyxyn = boxes.xyxyn.tolist()
     
-def is_cuda_available():
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected. Stopping the throbber and exiting...")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Stop the throbber animation
+        stop_trobber_event.set()
+        throbber_thread.join()
+        print("Program exited")
+        
+def is_cuda_available() -> bool:
     # Check if CUDA is available
     return torch.cuda.is_available()
     
-
+def throbber_animation(stop_event) -> None:
+    throbber = ["|", "/", "-", "\\"]
+    count = 0
+    while not stop_event.is_set():
+        print("AI Tracking " + throbber[count % len(throbber)], end="\r")
+        count += 1
+        time.sleep(0.1)
+        
+    print("throbber animation stopped")
 
 if __name__ == '__main__':
     main()
